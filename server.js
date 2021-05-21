daa881f3ada0fcc42082090accf0a3d21bc7411e
Jen Janik <jenmjanik@gmail.com>
2021-05-21 09:59:54 -0500

    updated server.js file for heroku deployment

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const app = express();
 
 // Serve only the static files from the dist directory
 // Replace angular-recipes-frontend with your project name
-app.use(express.static(__dirname + '/dist/apocamart-frontend'));
+app.use(express.static(__dirname + '/dist/marketplace-frontend'));
 
 app.get('/*', function(req,res) {
 
   // Replace angular-recipes-frontend with your project name
-  res.sendFile(path.join(__dirname+'/dist/apocamart-frontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/marketplace-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
